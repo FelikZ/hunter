22c445bc4ab4e6ce3dd61c517272d31944d9f2a6
FelikZ <thefelikz@gmail.com>
2018-08-27 14:05:34 +0200

    WIP: add websocketpp v0.8.1

--- cmake/projects/websocketpp/hunter.cmake
+++ cmake/projects/websocketpp/hunter.cmake
@@ -38,6 +38,17 @@ hunter_add_version(
   8ca868bd8c5290643574632aa72931280e497ce6
   )
 
+hunter_add_version(
+  PACKAGE_NAME
+  websocketpp
+  VERSION
+  "0.8.1-p0"
+  URL
+  "https://github.com/FelikZ/websocketpp/archive/v0.8.1-p0.tar.gz"
+  SHA1
+  5ccdaa626414c668719f9f9a85b0428ca71f232c
+  )
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(websocketpp)
 hunter_download(PACKAGE_NAME websocketpp)
